d76e62eae0dc1401f1765150373fa9538871725c
Healdb <healdb@healdb.com>
2015-09-14 12:01:35 -0400

    Create Shape.java

---
+++ Shapes/Shape.java
@@ -0,0 +1,9 @@
+public class Shape {
+     private int numSides;
+     public Shape(int numSides){
+         this.numSides=numSides;
+     }
+     public int getNumSides(){
+         return numSides;
+     }
+}
